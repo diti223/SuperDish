0d5035020e1126245a109d6f4baaccc480b02366
Adrian Bilescu <adrian.bilescu@gmail.com>
2024-03-14 15:02:09 +0200

    Removed all components except App & UseCase definition

--- SuperDish/SuperDishApp.swift
+++ SuperDish/SuperDishApp.swift
@@ -10,24 +10,10 @@ import MainDish
 
 @main
 struct SuperDishApp: App {
-
-    #if DEBUG
-    @StateObject var appFlow = DummyAppFlowFactory.makeAppFlow()
-    #else
-    @StateObject var appFlow = InMemoryAppFlowFactory.makeAppFlow()
-    #endif
-    @Environment(\.scenePhase) var scenePhase
     
     var body: some Scene {
         WindowGroup {
-            if let viewModel = appFlow.loginViewModel {
-                LoginView(viewModel: viewModel)
-            }
-        }
-        .onChange(of: scenePhase) { (oldPhase, newPhase) in
-            if case .active = newPhase {
-                appFlow.start()
-            }
+            Color.red
         }
     }
 }
